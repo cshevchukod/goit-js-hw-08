1cbb15de99de1ef88aa36261b959ae22f4e00e9c
Kostiantyn Shevchuk <constantine.shevchuk@gmail.com>
2026-05-18 00:37:26 +0300

    point6+point7

--- js/gallery.js
+++ js/gallery.js
@@ -96,5 +96,9 @@ function onGalleryItemClick(event) {
 
   const largeImageURL = event.target.dataset.source;
 
-  console.log(largeImageURL);
+  const instance = basicLightbox.create(`
+    <img src="${largeImageURL}" width="1112" height="640">
+  `);
+
+  instance.show();
 }
